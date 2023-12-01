8e526cb943199f0ef3c4cd168714abf9df34a570
Jasper Hsiao <orz2931@gmail.com>
2023-12-01 10:36:27 +0800

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navigation from "@/components/navigation/navigation";
+import Footer from "@/components/footer/footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className=" h-full w-full">
         <Navigation />
         <div>{children}</div>
+        <Footer />
       </body>
     </html>
   );
