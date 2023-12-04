64ffea8bcb2c089796a2914fc166cecd2785b04b
Jasper Hsiao <orz2931@gmail.com>
2023-12-04 20:49:46 +0800

    add initial nav

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,11 +1,25 @@
 import Link from "next/link";
-export default function Navigation(): JSX.Element {
+import Image from "next/image";
+export default function Navigation(): React.ReactNode {
   return (
-    <div className=" ">
-      <div>
-        <Link href="/">Logo</Link>
+    <div className="flex flex-row items-center justify-between">
+      <div className="mt-4 ml-4">
+        <Link href="/" className="flex flex-row items-center">
+          <Image
+            src="/logo.jpg"
+            alt="logo"
+            sizes="100px"
+            width={200}
+            height={200}
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          />
+          <p className="text-xs lg:text-3xl">享家蔬果園</p>
+        </Link>
       </div>
-      <div>
+      <div className="hidden lg:flex lg:flex-row lg:justify-around items-center mr-4 lg:space-x-4 lg:text-lg">
         <Link href="/about">關於我們</Link>
         <Link href="/shop">購買產品</Link>
         <Link href="/contact">聯絡我們</Link>
