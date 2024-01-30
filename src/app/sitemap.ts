5da5dec508cb3f42c59c11a4256ac40b9388f9b1
Jasper Hsiao <orz2931@gmail.com>
2024-01-30 18:53:02 +0800

    fix wrong word

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "hhttps://e-shop-client-alpha.vercel.app/",
+      url: "https://e-shop-client-alpha.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
